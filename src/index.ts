285ff4045b9f17ade30f0e12f77aec08cc5ee45b
Kenton Vizdos <kvizdos@protonmail.com>
2025-05-08 00:17:51 -0400

    export..

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from "./forms/login.component";
 export * from "./forms/registration.component";
+export * from "./forms/reset-password.component";
 export * from "./helpers/fingerprint";
 export * from "./layout/locksmith.layout";
 export * from "./styles/inputs.style";
