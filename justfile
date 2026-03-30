8f7c01d1e4e6ac3979f1e236749f0cb1f15906a6
Kenton Vizdos <kvizdos@protonmail.com>
2026-03-30 11:24:12 -0400

    Bundle

--- justfile
+++ justfile
@@ -3,3 +3,4 @@ default: build
 build:
     npm run build
     npm run bundle
+    echo "Did you add any new files to index.ts? Gotta do that to push an update"
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export * from "./forms/error.component";
 export * from "./forms/registration.component";
 export * from "./forms/reset-password.component";
 export * from "./forms/profile.component";
+export * from "./forms/verify-email.component";
 export * from "./helpers/fingerprint";
 export * from "./layout/locksmith.layout";
 export * from "./styles/inputs.style";
